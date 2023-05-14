d4b039290be291520ef744d860ffe532dc5f82ab
atul-9 <atul09agrawal@gmail.com>
2023-05-14 18:42:48 +0530

    backenddb_Working

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mysql= require("mysql")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "xyz",
+    database: "demoproject",
+  });
+connection.connect((err) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("connected to Database");
+    }
+});
+
+
+module.exports= connection;
+
---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require( "express");
+
+const connection = require("./db.js")
+const app=express();
+
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.json("Hello Atul")
+})
+
+app.get('/data',(req,res)=>{
+    const q= "select * from login "
+    connection.query(q,(err,data)=>{
+        if(err) 
+            return res.json(err)
+        return res.json(data)
+    })
+
+})
+/*
+app.post('/add',(req,res)=>{
+    const q= "insert into demoproject.login values(?)"
+    const value=[req.body.email,req.body.password,req.body.sec,req.body.ans,req.body.role]
+    connection.query(q,[value],(err,data)=>{
+        if(err) 
+            return res.json(err)
+        return res.json(data)
+    })
+})
+*/
+app.listen(8800,()=>{
+    console.log("Connected to backend")
+});
